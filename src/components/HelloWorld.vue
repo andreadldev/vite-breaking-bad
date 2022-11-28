1fbd04e8efe0ea55b5df354bab61c59d40d3ef67
andreadldev <andrea.delisi2@gmail.com>
2022-11-28 16:29:33 +0100

    --

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'HelloWorld',
+}
+</script>
+
+<template>
+    <div class="vh-100 d-flex justify-content-center align-items-center">
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
